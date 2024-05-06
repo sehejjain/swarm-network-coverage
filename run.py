6ae4e0ff7a40cbf59b4a0c5897e3b41e3a885b77
Sehej Jain <sehejjain@outlook.com>
2024-05-06 04:07:26 -0400

    run params

--- run.py
+++ run.py
@@ -12,7 +12,6 @@ from env import NetworkEnv
 from utils import EpisodicRewardLogger
 
 logging.getLogger().setLevel(logging.INFO)
-print("hello")
 
 
 
@@ -26,12 +25,12 @@ step = 1
 radius = 20
 render_mode = "rgb_array"
 max_ep_len = 5000
-bonus_threshold = 0.9
-bonus_reward = 1000
+bonus_threshold = 0.6
+bonus_reward = 100
 overlap_threshold = 0.1
 
 
-def get_env():
+def get_env(train=True):
     """Return env"""
     return NetworkEnv(
         n,
@@ -44,31 +43,36 @@ def get_env():
         bonus_threshold=bonus_threshold,
         bonus_reward=bonus_reward,
         overlap_threshold=overlap_threshold,
+        train=train,
     )
 
 
 env = make_vec_env(get_env, n_envs=n_envs)
 
-checkpoint_callback = CheckpointCallback(save_freq=100_000, save_path="./logs/")
+checkpoint_callback = CheckpointCallback(save_freq=100_000, save_path="./logs/ppo4/")
 
-eval_env = Monitor(get_env())
+eval_env = Monitor(get_env(train=False))
 
-eval_callback = EvalCallback(eval_env, best_model_save_path="./logs/best_model",
-                             log_path="./logs/results", eval_freq=100_000)
+eval_callback = EvalCallback(eval_env, best_model_save_path="./logs/ppo4/best_model",
+                             log_path="./logs/ppo4/results", eval_freq=50_000)
 
 callback = CallbackList([checkpoint_callback, eval_callback, EpisodicRewardLogger()])
 
 
-model1 = PPO("MultiInputPolicy", env, verbose=0, tensorboard_log="tensorboard_logs/")
+model1 = PPO("MultiInputPolicy", env, verbose=0, tensorboard_log="tensorboard_logs/", learning_rate=1e-5)
 
 logging.info("Start training")
 logging.info("Device %a", model1.device)
+
+# model1.load("logs/best_model/best_model")
+
 start=time.time()
 model1.learn(
-    total_timesteps=10_000,
+    total_timesteps=10_000_000,
     callback=callback,
-    tb_log_name="ppo1",
+    tb_log_name="ppo4",
     progress_bar=True,
     reset_num_timesteps=False,
 )
 print("Time taken: ", time.time()-start)
+
